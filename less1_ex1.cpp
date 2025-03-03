b4e1d5d6853a9c8708ca01f9b6251e2c4438d06f
G0shan4ik <egorkononec64@gmail.com>
2025-03-03 22:43:47 +0300

    [F] fix

--- less1_ex1.cpp
+++ less1_ex1.cpp
@@ -2,18 +2,34 @@
 using namespace std;
 
 int main() {
-    long long n, k;
+    long n, k;
     cin >> n >> k;
     
-    if (k < 3 || k > 2 * n - 1) {
+    if(k < 3 || k > 2*n - 1) {
         cout << 0;
         return 0;
     }
     
-    long long L = max(1LL, k - n);
-    long long R = min((k - 1) / 2, n - 1);
+
+    long min_first; 
+    if(k - n > 1) {
+        min_first = k - n; 
+    } else {
+        min_first = 1; 
+    }
     
-    cout << (L > R ? 0 : R - L + 1);
+    long max_first;
+    if((k-1)/2 < n-1) {
+        max_first = (k-1)/2; 
+    } else {
+        max_first = n-1; 
+    }
+    
+    if(min_first > max_first) {
+        cout << 0;
+    } else {
+        cout << (max_first - min_first + 1);
+    }
     
     return 0;
 }
