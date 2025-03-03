04b1b5888d6a31c4a7342d86b64274fc3f2db4bf
G0shan4ik <egorkononec64@gmail.com>
2025-03-03 21:05:05 +0300

    [-] fatal merge commit test2 branch

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void print_num(short num){
-    cout << num << endl;
+void print_num(short number){
+    cout << "Goodbay world - " << number << endl;
 }
 
 int main() {
-    cout << "Goodbay world" << endl;
+    print_num(1000);
     return 0;
 }
